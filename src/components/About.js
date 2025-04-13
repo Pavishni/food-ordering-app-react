523e3ec0b7164da46d77294a734f98d9a75e3212
Pavishni <pavishni.r@gmail.com>
2025-04-13 23:07:19 +0530

    Added React routers

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+     <div>
+        <h1>About US</h1>
+        <h2>React Training of food ordering site from Swiggy API</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () =>{
+    return (
+        <div>
+            <h1>Contact US!</h1>
+            <h3>Mobile = 1234567890</h3>
+        </div>
+    )
+}
+
+export default Contact;
