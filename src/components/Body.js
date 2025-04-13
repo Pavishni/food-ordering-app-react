3e2967e2fc72c8134af6230934f7e43127ee1658
Pavishni <pavishni.r@gmail.com>
2025-04-13 17:37:14 +0530

    Added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,83 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    const [listOfRestaurants, setRestaurants] = useState(resList);
-    return (
-        <div className="body">
-            <div className="filter">
-                <button className="filter-btn"
-                    onClick={() => {
-                        const filteredList = listOfRestaurants.filter(
-                            (res) => res.info.avgRating >= 4.5
-                        );
-                        setRestaurants(filteredList);
-                    }}>
-                    Top Rated Restaurants
-                    </button>
-            </div>
-            <div className="res-container">
-                {
-                    listOfRestaurants.map((restaurant) => (
-                        <RestaurantCard key={restaurant.info.id} resData={restaurant} />
-                    ))}
-            </div>
+  const [listOfRestaurants, setRestaurants] = useState([]);
+  const [filteredRestaurantList, setFilteredRestaurants] = useState([]);
+  const [searchRestaurants, setSearchRestaurants] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0843007&lng=80.2704622&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    setRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchRestaurants}
+            onChange={(e) => {
+              setSearchRestaurants(e.target.value);
+              const filteredRestaurants = listOfRestaurants.filter((res) =>
+                res.info.name
+                  .toLowerCase()
+                  .includes(searchRestaurants.toLowerCase())
+              );
+              setFilteredRestaurants(filteredRestaurants);
+            }}
+          />
+          <button
+            className="search-button"
+            onClick={() => {
+              const filteredRestaurants = listOfRestaurants.filter((res) =>
+                res.info.name
+                  .toLowerCase()
+                  .includes(searchRestaurants.toLowerCase())
+              );
+              setFilteredRestaurants(filteredRestaurants);
+            }}
+          >
+            Search
+          </button>
         </div>
-    )
-}
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating >= 4.5
+            );
+            setFilteredRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestaurantList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
---
+++ src/components/Shimmer.js
@@ -0,0 +1,16 @@
+const Shimmer = () => {
+    return (
+        <div className = "shimmer-container">
+            <div className= "shimmer-cards" />
+            <div className= "shimmer-cards" />
+            <div className= "shimmer-cards" />
+            <div className= "shimmer-cards" />
+            <div className= "shimmer-cards" />
+            <div className= "shimmer-cards" />
+            <div className= "shimmer-cards" />
+            <div className= "shimmer-cards" />
+        </div>
+    );
+};
+
+export default Shimmer;
