3bd00d390e986d8ead5e9db5f3c74675a57e6f14
Pavishni <pavishni.r@gmail.com>
2025-04-16 22:36:11 +0530

    class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
      <div>
         <h1>About US</h1>
         <h2>React Training of food ordering site from Swiggy API</h2>
+        <User name = {"Pavi"} location = {"Chennai"}/>
+        <UserClass name = {"Pavi"} location = {"Chennai"}/>
     </div>
   );
 };
