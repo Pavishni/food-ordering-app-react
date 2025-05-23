248888a157472a3766ce6e4423cfe6bc9ce11c87
Pavishni <pavishni.r@gmail.com>
2025-05-23 22:56:55 +0530

    added firebase configs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@ export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-let
 
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const RES_URL = "https://proxy.corsfix.com/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0843007&lng=80.2704622&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const RES_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0843007&lng=80.2704622&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-export const RES_MENU_URL = "https://proxy.corsfix.com/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0843007&lng=80.2704622&restaurantId=";
+export const RES_MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0843007&lng=80.2704622&restaurantId=";
